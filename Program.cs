764dbeb411cee6eba16deddb4945f4ea9eb4c22b
Caroline Zengo <carolinezengo@gmail.com>
2023-11-13 15:06:54 -0300

    Desafio Concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
+
+Console.WriteLine("Smartphone Iphone:");
+Iphone iphone = new Iphone(numero : "123654477", modelo: "Modelo1", imei :"1122552236", memoria: 64);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facebook");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Nokia:");
+Nokia nokia = new Nokia(numero : "789456123", modelo: "Modelo20", imei :"112252225", memoria: 32);
+nokia.Ligar();
+
+nokia.InstalarAplicativo("Twitter");
 // TODO: Realizar os testes com as classes Nokia e Iphone
